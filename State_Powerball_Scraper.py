49c9e8b32891d265ead8be7cb401b8cf320bf931
MouZer77 <skymouser@gmail.com>
2023-10-29 12:02:22 -0400

    Updating ReadME and adding a new scraper for another powerball site

--- State_Powerball_Scraper.py
+++ State_Powerball_Scraper.py
@@ -1,20 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://powerball.us.org/lottery-powerball-jackpot-winners-list-by-state"
+url = "https://www.powerball.net/winners"
 response = requests.get(url)
 html_content = response.content
 soup = BeautifulSoup(html_content, "html.parser")
 # find table object:
-table = soup.find('table', id="winnerstable")
+table = soup.find('table', id="table-winners")
 # iterate through each row:
-header = [] #Gives Headers a place to be placed for organization
-rows = [] #Gives Rows a place to be placed for organization
-for i, row in enumerate(table.find_all('tr')):
+header = []
+rows = []
+for i, row in enumerate(soup.find_all('tr')):
     if i == 0:
         header = [el.text.strip() for el in row.find_all('th')]
     else:
         rows.append([el.text.strip() for el in row.find_all('td')])
+
+divs = soup.find('div', text='')
+
+for div in divs:
+    div.extract()
 
 print(header)
 for row in rows:
