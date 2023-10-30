fe73d3a3bc815dc0314cd714d11a7cedc5948df0
MouZer77 <skymouser@gmail.com>
2023-10-30 11:57:26 -0400

    Added new scrapers, converted scraper data to CSV

--- Powerball_Win_Values_Scraper.py
+++ Powerball_Win_Values_Scraper.py
@@ -2,16 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-url = "https://powerball.us.org/lottery-powerball-jackpot-winners-list-by-state"
+url = "https://www.powerball.net/winners"
 response = requests.get(url)
 html_content = response.content
 soup = BeautifulSoup(html_content, "html.parser")
-# find table object:
-table = soup.find('table', id="winnerstable")
-# iterate through each row:
-header = [] #Gives Headers a place to be placed for organization
-rows = [] #Gives Rows a place to be placed for organization
-for i, row in enumerate(table.find_all('tr')):
+
+tab = soup.find("table",{"class":"mobFormat"})
+
+header = []
+rows = []
+
+for i, row in enumerate(tab('tr')):
     if i == 0:
         header = [el.text.strip() for el in row.find_all('th')]
     else:
@@ -26,6 +27,6 @@ result.append(header)
 for row in rows:
     result.append(row)
 
-with open('Powerball.csv', 'w', newline='') as file:
+with open('Powerball_Win_values.csv', 'w', newline='') as file:
     writer = csv.writer(file)
     writer.writerows(result)
--- State_Powerball_Scraper.py
+++ State_Powerball_Scraper.py
@@ -1,26 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 url = "https://www.powerball.net/winners"
 response = requests.get(url)
 html_content = response.content
 soup = BeautifulSoup(html_content, "html.parser")
 # find table object:
-table = soup.find('table', id="table-winners")
-# iterate through each row:
+tab = soup.find("table",{"class":"table-winners"})
+
 header = []
 rows = []
-for i, row in enumerate(soup.find_all('tr')):
+
+for i, row in enumerate(tab('tr')):
     if i == 0:
         header = [el.text.strip() for el in row.find_all('th')]
     else:
         rows.append([el.text.strip() for el in row.find_all('td')])
 
-divs = soup.find('div', text='')
-
-for div in divs:
-    div.extract()
-
-print(header)
+result = []
+result.append(header)
 for row in rows:
-    print(row)
+    result.append(row)
+
+with open('State_Powerball.csv', 'w', newline='') as file:
+    writer = csv.writer(file)
+    writer.writerows(result)
