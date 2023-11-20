03884d30b4744d685f1081bab7ef2c1308c3e02a
MouZer77 <134120843+MouZer77@users.noreply.github.com>
2023-11-20 12:32:31 -0500

    Issue with upload

--- Stat_Scraper.py
+++ Stat_Scraper.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -30,37 +29,3 @@ for row in rows:
 
 with open('Lottery_Sales_Stats.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-=======
-import requests
-from bs4 import BeautifulSoup
-import csv
-
-url = "https://www.statista.com/statistics/388238/sales-of-lotteries-by-state-us/"
-response = requests.get(url)
-html_content = response.content
-soup = BeautifulSoup(html_content, "html.parser")
-
-tab = soup.find("table",{"class":"table hidden"})
-
-header = []
-rows = []
-
-for i, row in enumerate(tab('tr')):
-    if i == 0:
-        header = [el.text.strip() for el in row.find_all('th')]
-    else:
-        rows.append([el.text.strip() for el in row.find_all('td')])
-
-# print(header)
-# for row in rows:
-#     print(row)
-
-result = []
-result.append(header)
-for row in rows:
-    result.append(row)
-
-with open('Lottery_Sales_Stats.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
->>>>>>> 2b512835f280dcbecb1c1a7dc31d9531d45013f2
-    writer.writerows(result)
