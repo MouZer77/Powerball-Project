d0f1bdb46e07b65bf2400a2881cb3529e6d7ae1f
MouZer77 <134120843+MouZer77@users.noreply.github.com>
2023-11-20 12:38:44 -0500

    Fixing an issue upload caused

--- Wealth_Inequality_Scraper.py
+++ Wealth_Inequality_Scraper.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests
 from bs4 import BeautifulSoup
 import csv
@@ -30,37 +29,3 @@ for row in rows:
 
 with open('Wealth_Inequality.csv', 'w', newline='') as file:
     writer = csv.writer(file)
-=======
-import requests
-from bs4 import BeautifulSoup
-import csv
-
-url = "https://www.statista.com/statistics/227249/greatest-gap-between-rich-and-poor-by-us-state/#:~:text=New%20York%20was%20the%20state,the%20United%20States%20that%20year."
-response = requests.get(url)
-html_content = response.content
-soup = BeautifulSoup(html_content, "html.parser")
-
-tab = soup.find("table",{"class":"table hidden"})
-
-header = []
-rows = []
-
-for i, row in enumerate(tab('tr')):
-    if i == 0:
-        header = [el.text.strip() for el in row.find_all('th')]
-    else:
-        rows.append([el.text.strip() for el in row.find_all('td')])
-
-# print(header)
-# for row in rows:
-#     print(row)
-
-result = []
-result.append(header)
-for row in rows:
-    result.append(row)
-
-with open('Wealth_Inequality.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
->>>>>>> 2b512835f280dcbecb1c1a7dc31d9531d45013f2
-    writer.writerows(result)
